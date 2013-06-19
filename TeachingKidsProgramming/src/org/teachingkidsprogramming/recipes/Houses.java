a5aa868e0b3ac2cc370bc050889f6c3f78cfba12
Tanner Saffert <Tsaffert@hotmail.com>
2013-06-19 15:10:31 -0700

    Got rid of comments

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -7,23 +7,15 @@ public class Houses
 {
   public static void main(String[] args)
   {
-    //   Make the tortoise move as fast as possible --#11
-    //   Have the tortoise start at 200 pixels in on the X axis --#14
+
     Tortoise.setSpeed(10);
     Tortoise.setX(200);
     Tortoise.setPenColor(Colors.Grays.LightGray);
     int height = 50;
     drawHouse(height);
     drawHouse(120);
-    //   The current height is 40 --#1.2
-    //   drawHouse (recipe below) --#9
-    //   ------------- Recipe for drawHouse --#9
-    //   Change the color of the line the tortoise draws to lightGray --#15
-    //   ------------- End of drawHouse recipe
-    //   DrawHouse with height 90 (recipe below) --#12
-    drawHouse(90);
+    drawHouse(120);
     drawHouse(20);
-    //   DrawHouse with height 20 (recipe below) --#13
   }
   private static void drawHouse(int height)
   {
