6911da6bb1f523b29f074ad431401e77c549deed
Tanner Saffert <Tsaffert@hotmail.com>
2013-06-18 21:27:56 -0700

    Testing out lettermaking

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,21 +1,34 @@
 package org.teachingkidsprogramming.recipes;
 
-import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
 public class SimpleSquare
 {
+  public static float Lettersize = 50;
   public static void main(String[] args) throws Exception
   {
-    Tortoise.setSpeed(10);
-    int sides = 360 / 3;
-    while (true)
-    {
-      Tortoise.setPenWidth(2);
-      Tortoise.setPenColor(Colors.getRandomColor());
-      Tortoise.setPenWidth(1);
-      Tortoise.move(100);
-      Tortoise.turn(sides - 1);
-    }
+    //All numbers start at the bottom left
+    Tortoise.setSpeed(2);
+    LetterB();
+  }
+  public static void LetterA()
+  {
+    //Makes the letter A
+    Tortoise.setAngle(45);
+    Tortoise.move(Lettersize);
+    Tortoise.setAngle(135);
+    Tortoise.move(Lettersize);
+    Tortoise.setAngle(315);
+    Tortoise.move(Lettersize / 2);
+    Tortoise.setAngle(315 - 45);
+    Tortoise.move(35.35);
+  }
+  public static void LetterB()
+  {
+    Tortoise.move(Lettersize);
+    Tortoise.setAngle(90);
+    Tortoise.move(Lettersize / 1.5);
+    Tortoise.setAngle(180);
+    Tortoise.move(Lettersize / 2);
   }
 }
