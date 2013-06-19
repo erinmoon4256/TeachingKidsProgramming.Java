fd89f9132519486cbb2351a62c781240cb5efd5c
Tanner Saffert <Tsaffert@hotmail.com>
2013-06-19 15:23:10 -0700

    Color changed to grey

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -7,18 +7,17 @@ public class Houses
 {
   public static void main(String[] args)
   {
-
     Tortoise.setSpeed(10);
     Tortoise.setX(200);
-    Tortoise.setPenColor(Colors.Grays.LightGray);
     int height = 50;
     drawHouse(height);
     drawHouse(120);
-    drawHouse(120);
+    drawHouse(90);
     drawHouse(20);
   }
   private static void drawHouse(int height)
   {
+    Tortoise.setPenColor(Colors.Grays.LightGray);
     Tortoise.move(height);
     Tortoise.turn(90);
     Tortoise.move(30);
