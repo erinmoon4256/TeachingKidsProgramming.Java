e41849c55d315cfb13d3aaca30248281856e7638
Tanner Saffert <Tsaffert@hotmail.com>
2013-06-18 19:31:54 -0700

    Trying to make a triangle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,16 +1,21 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
 
 public class SimpleSquare
 {
   public static void main(String[] args) throws Exception
   {
-    //  Show the tortoise --#1
-    //  Make the tortoise move as fast as possible --#6
-    //  Do the following 4 times --#5.1
-    //      Change the color of the line the tortoise draws to "blue" --#4
-    //      Move the tortoise 50 pixels --#2
-    //      Turn the tortoise to the right (90 degrees) --#3
-    //  Repeat --#5.2
+    Tortoise.setSpeed(10);
+    int sides = 360 / 3;
+    while (true)
+    {
+      Tortoise.setPenWidth(2);
+      Tortoise.setPenColor(Colors.getRandomColor());
+      Tortoise.setPenWidth(1);
+      Tortoise.move(100);
+      Tortoise.turn(sides - 1);
+    }
   }
 }
