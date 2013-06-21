68daffebad025f1a772209468ff9580b39c90b2c
Erin Moon <kongjui.m@hotmail.com>
2013-06-20 22:07:42 -0700

    idkwhatthisis

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,13 +1,19 @@
 package org.teachingkidsprogramming.recipes;
 
+<<<<<<< HEAD
 import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.windows.MessageBox;
+=======
+import org.teachingextensions.logo.Tortoise;
+>>>>>>> 15e40837d3dff52804d6d3938f1e55bf3e07d308
 
 public class SimpleSquare
 {
+  public static float Lettersize = 50;
   public static void main(String[] args) throws Exception
   {
+<<<<<<< HEAD
     Tortoise.show();
     Tortoise.setSpeed(10);
     int sides = MessageBox.askForNumericalInput("How many boyfriends do you have??");
@@ -18,5 +24,33 @@ public class SimpleSquare
       Tortoise.move(i * -5);
       Tortoise.turn(360.0 * 3 / sides);
     }
+=======
+    //All numbers start at the bottom left
+    Tortoise.setSpeed(2);
+    LetterB();
+  }
+  public static void LetterA()
+  {
+    //Makes the letter A
+    Tortoise.setAngle(45);
+    Tortoise.move(Lettersize);
+    Tortoise.setAngle(135);
+    Tortoise.move(Lettersize);
+    Tortoise.setAngle(315);
+    Tortoise.penUp();
+    Tortoise.move(Lettersize / 2);
+    Tortoise.setAngle(315 - 45);
+    Tortoise.penDown();
+    Tortoise.move(35.35);
+  }
+  public static void LetterB()
+  {
+    Tortoise.move(Lettersize);
+    Tortoise.setAngle(90);
+    Tortoise.move(Lettersize / 1.52);
+    Tortoise.setAngle(180);
+    Tortoise.move(Lettersize / 2);
+    Tortoise.setAngle(0);
+>>>>>>> 15e40837d3dff52804d6d3938f1e55bf3e07d308
   }
 }
